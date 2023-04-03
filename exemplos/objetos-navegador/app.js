29584d9839de75a38f98b1618c0c3772be574095
Francisco Dantas <dantas.nobre@ifpb.edu.br>
2023-04-03 16:02:23 -0300

    Exemplo de elementos dinâmicos

---
+++ exemplos/objetos-navegador/app.js
@@ -0,0 +1,16 @@
+import disciplinas from './model/disciplinas.js';
+
+for (const disciplina of disciplinas) {
+  let result = `
+    <section>
+      <h2>${disciplina.nome}</h2>
+      <p>Carga horária: ${disciplina.cargaHoraria}</p>
+      <p>Sala: ${disciplina.local}</p>
+      <hr>
+    </section>
+  `;
+
+  document.querySelector('script').insertAdjacentHTML('beforebegin', result);
+}
+
+// console.log(disciplinas);
---
+++ exemplos/objetos-navegador/model/disciplinas.js
@@ -0,0 +1,19 @@
+const disciplinas = [
+  {
+    nome: 'Linguagens de Script',
+    cargaHoraria: '80h',
+    local: 'Redes Convergentes',
+  },
+  {
+    nome: 'Estrutura de Dados',
+    cargaHoraria: '100h',
+    local: 'Lab 57',
+  },
+  {
+    nome: 'Português',
+    cargaHoraria: '40h',
+    local: 'Sala de aula 12',
+  },
+];
+
+export default disciplinas;
