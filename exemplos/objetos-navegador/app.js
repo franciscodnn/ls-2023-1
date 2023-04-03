3b31c49b161e1c055824fddea5d3f07aef6277a0
Francisco Dantas <dantas.nobre@ifpb.edu.br>
2023-04-03 16:12:26 -0300

    Criação da função de carga de dados

--- exemplos/objetos-navegador/app.js
+++ exemplos/objetos-navegador/app.js
@@ -1,16 +1,20 @@
 import disciplinas from './model/disciplinas.js';
 
-for (const disciplina of disciplinas) {
-  let result = `
-    <section>
-      <h2>${disciplina.nome}</h2>
-      <p>Carga horária: ${disciplina.cargaHoraria}</p>
-      <p>Sala: ${disciplina.local}</p>
-      <hr>
-    </section>
-  `;
+loadData();
 
-  document.querySelector('script').insertAdjacentHTML('beforebegin', result);
+function loadData() {
+  for (const disciplina of disciplinas) {
+    let result = `
+      <section>
+        <h2>${disciplina.nome}</h2>
+        <p>Carga horária: ${disciplina.cargaHoraria}</p>
+        <p>Sala: ${disciplina.local}</p>
+        <hr>
+      </section>
+    `;
+
+    document.querySelector('script').insertAdjacentHTML('beforebegin', result);
+  }
 }
 
 // console.log(disciplinas);
