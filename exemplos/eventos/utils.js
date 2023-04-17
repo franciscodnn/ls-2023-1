daaa6aa8bea9cc556436bf43c25e720df3e0baec
Francisco Dantas <dantas.nobre@ifpb.edu.br>
2023-04-17 16:02:28 -0300

    Apresentação dos dados na tag P

--- exemplos/eventos/utils.js
+++ exemplos/eventos/utils.js
@@ -4,9 +4,42 @@ function tratarClique() {
   const inputMatricula = document.querySelector('#matricula');
 
   console.log(
-    `${inputNome.value}, ${inputEmail.value}, ${inputMatricula.vallue}`
+    `${inputNome.value}, ${inputEmail.value}, ${inputMatricula.value}`
   );
   inputNome.value = '';
   inputEmail.value = '';
   inputMatricula.value = '';
 }
+
+function imprimirDados() {
+  const inputNome = document.querySelector('#nome');
+  const inputEmail = document.querySelector('#email');
+  const inputMatricula = document.querySelector('#matricula');
+  const inputModalidadeCurso = document.querySelector(
+    'input[type="radio"]:checked'
+  );
+
+  const resposta = document.querySelector('#resposta');
+
+  resposta.innerHTML = `${inputNome.value}, ${inputEmail.value}, ${inputMatricula.value}, ${inputModalidadeCurso.value}`;
+
+  inputNome.value = '';
+  inputEmail.value = '';
+  inputMatricula.value = '';
+}
+
+document.querySelector('form').addEventListener('keypress', function (event) {
+  console.log(event);
+
+  if (event.key == 'Enter') {
+    imprimirDados();
+  }
+});
+
+document.querySelector('button').addEventListener('click', function () {
+  imprimirDados();
+});
+
+document.querySelector('button').addEventListener('click', function () {
+  alert('Botão clicado!');
+});
