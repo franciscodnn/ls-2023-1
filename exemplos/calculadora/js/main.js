b3688b691fbc13a79cae6524825550b5cfd8319e
Francisco Dantas <dantas.nobre@ifpb.edu.br>
2023-04-19 16:23:57 -0300

    Calculadora atualizado com a operação de soma

--- exemplos/calculadora/js/main.js
+++ exemplos/calculadora/js/main.js
@@ -1,5 +1,34 @@
-document.querySelectorAll('.digits.flex > button').forEach((botao) => {
-  botao.addEventListener('click', function (evento) {
-    console.log(evento.target.innerHTML);
+function loadEvents() {
+  document.querySelectorAll('.digits.flex > button').forEach((botao) => {
+    botao.addEventListener('click', function (evento) {
+      const visor = document.querySelector('.wrapper h1');
+
+      if (visor.innerHTML == '0') visor.innerHTML = '';
+
+      visor.innerHTML += evento.target.innerHTML;
+    });
   });
-});
+
+  document.querySelectorAll('.operations.subgrid > button').forEach((botao) => {
+    botao.addEventListener('click', function (evento) {
+      const visor = document.querySelector('.wrapper h1');
+
+      if (evento.target.innerHTML == '=') {
+        const resultado = calcular();
+        visor.innerHTML = resultado;
+      } else {
+        visor.innerHTML += evento.target.innerHTML;
+      }
+    });
+  });
+}
+
+function calcular() {
+  const visor = document.querySelector('.wrapper h1');
+
+  let operandos = visor.innerHTML.split('+');
+
+  let resultado = Number(operandos[0]) + Number(operandos[1]);
+
+  return resultado;
+}
