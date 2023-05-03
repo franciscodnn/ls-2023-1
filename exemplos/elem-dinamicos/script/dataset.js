08dc4fa9e21e0bf9254250fddf03575390987358
Francisco Dantas <dantas.nobre@ifpb.edu.br>
2023-05-03 14:47:54 -0300

    Atualização do repositório

---
+++ exemplos/elem-dinamicos/script/dataset.js
@@ -0,0 +1,28 @@
+const cursos = [
+  {
+    id: 1,
+    nome: 'Sistemas para Internet',
+    cargaHoraria: '2800h',
+    instituicao: 'IFPB',
+    tipoCurso: 'Tecnólogo',
+    imagem: 'none',
+  },
+  {
+    id: 2,
+    nome: 'Engenharia Elétrica',
+    cargaHoraria: '3200h',
+    instituicao: 'IFPB',
+    tipoCurso: 'Bacharelado',
+    imagem: 'none',
+  },
+  {
+    id: 3,
+    nome: 'Redes de Computadores',
+    cargaHoraria: '2600h',
+    instituicao: 'IFPB',
+    tipoCurso: 'Tecnólogo',
+    imagem: 'none',
+  },
+];
+
+export default cursos;
