bc1a89a0054f1a4bf8978d74e4037ad630a69233
Francisco Dantas <dantas.nobre@ifpb.edu.br>
2023-05-17 16:17:51 -0300

    Resolução do Fetch viaCEP

--- exemplos/fetch/js/main.js
+++ exemplos/fetch/js/main.js
@@ -6,5 +6,42 @@ async function getCEP(cep) {
   return data;
 }
 
-const informacoesCEP = await getCEP('58015435');
-console.log(informacoesCEP.logradouro);
+function preencherCEP(objJSON) {
+  const inputCEP = document.querySelector('#cep');
+  const inputRua = document.querySelector('#street');
+  const inputNumero = document.querySelector('#number');
+  const inputBairro = document.querySelector('#neighborhood');
+  const inputEstado = document.querySelector('#state');
+  const inputCidade = document.querySelector('#city');
+
+  if (objJSON != null) {
+    inputRua.value = objJSON.logradouro;
+    inputNumero.value = objJSON.complemento;
+    inputBairro.value = objJSON.bairro;
+    inputEstado.value = objJSON.uf;
+    inputCidade.value = objJSON.localidade;
+  } else {
+    inputRua.value = '';
+    inputNumero.value = '';
+    inputBairro.value = '';
+    inputEstado.value = '';
+    inputCidade.value = '';
+  }
+}
+
+document.querySelector('#cep').addEventListener('keyup', async (event) => {
+  const cep = event.target.value;
+
+  if (cep.length === 8) {
+    const data = await getCEP(cep);
+
+    preencherCEP(data);
+  } else {
+    preencherCEP(null);
+  }
+
+  // console.log();
+});
+
+// const informacoesCEP = await getCEP('58015435');
+// console.log(informacoesCEP.logradouro);
